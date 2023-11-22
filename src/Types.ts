111c739c249bb0313a7a2bbef619bb199269b22e
Carl Boquiren <carlboquiren@gmail.com>
2023-11-22 00:58:39 -0500

    Create Expected Types

---
+++ src/Types.ts
@@ -0,0 +1,58 @@
+export type TGame = {
+  name: string,
+  description: string,
+  "genre-0": string,
+  "genre-1"?: string,
+  "genre-2"?: string,
+  "genre-3"?: string,
+  "genre-4"?: string,
+  image: string,
+  developer: string,
+  "platform-0": string,
+  "platform-1"?: string,
+  "platform-2"?: string,
+  "platform-3"?: string,
+  "platform-4"?: string,
+  id: number
+}
+
+export type TUser = {
+  username: string,
+  password: string,
+  id: number
+}
+
+type TFavorite = {
+  userId: number,
+  gameId: number,
+  type: "favorite"
+  id: number,
+  isFavorite: boolean
+}
+
+type TPlayed = {
+  userId: number,
+  gameId: number,
+  type: "played",
+  id: number,
+  hasPlayed: boolean
+}
+
+type TOwned = {
+  userId: number,
+  gameId: number,
+  type: "owned",
+  id: number,
+  hasPlayed: boolean
+}
+
+type THidden = {
+  userId: number,
+  gameId: number,
+  type: "hidden",
+  id: number,
+  isHidden: boolean
+}
+
+
+export type TInteraction = TFavorite | TPlayed | TOwned | THidden;
